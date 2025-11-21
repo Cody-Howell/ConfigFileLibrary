13cf403fa02958210aadf434602bc95eef16401e
Cody <codyhowell44@gmail.com>
2025-11-20 19:13:59 -0700

    YAML files are extracted

--- ConfigFileLibrary/ConfigFileLibrary/ConfigFile.cs
+++ ConfigFileLibrary/ConfigFileLibrary/ConfigFile.cs
@@ -1,10 +1,11 @@
 
 
+using ConfigFileLibrary.Helpers;
 using ConfigFileLibrary.Primitives;
 
 public class ConfigFile {
     private IBaseConfigOption option;
-    
+
     /// <summary/>
     public IBaseConfigOption this[string key] => option[key];
     /// <summary/>
@@ -25,7 +26,7 @@ public class ConfigFile {
     public string AsString() => option.AsString();
     /// <summary/>
     public List<string> AsStringList() => option.AsStringList();
-    
+
     /// <summary/>
     public ConfigFile(string file) {
         string extension = Path.GetExtension(file);
@@ -33,8 +34,27 @@ public class ConfigFile {
             case ".txt":
                 option = ReadAsTXTFile(file);
                 return;
-            default: 
-                throw new Exception($"Extension {extension} was not known.");  
+            case ".yaml":
+            case ".yml":
+                List<(int indentCount, string data)> lines = YAMLHelper.ReturnOrderedLines(file);
+
+                if (lines[0].data.StartsWith('-')) {
+                    // Read lines as list
+                    option = ReadLinesAsList(lines);
+                } else if (lines[0].data.Contains(':')) {
+                    // Read lines as dictionary
+                    option = ReadLinesAsDictionary(lines);
+                } else {
+                    // Read lines as primitive
+                    string line = "";
+                    foreach ((int indentCount, string data) in lines) {
+                        line += data + " ";
+                    }
+                    option = ReadLineAsPrimitive(line);
+                }
+                break;
+            default:
+                throw new Exception($"Extension {extension} was not known.");
         }
         // option  = new PrimitiveConfigOption(" ");
     }
@@ -84,6 +104,80 @@ public class ConfigFile {
 
         string filename = Path.GetFileName(path);
         return new ObjectConfigOption(values, filename);
+    }
+    #endregion
+
+    #region YAML
+    private IBaseConfigOption ReadLinesAsDictionary(List<(int indentCount, string data)> lines) {
+        Dictionary<string, IBaseConfigOption> start = new Dictionary<string, IBaseConfigOption>();
+        int currentIndent = lines[0].indentCount;
+        for (int i = 0; i < lines.Count; i++) {
+            (int indentCount, string data) line = lines[i];
+            string[] splits = line.data.Split(':');
+            if (splits.Length > 2) {
+                throw new FormatException($"Don't include multiple (:) on the same line. I read key: \"{splits[0].Trim()}\"");
+            }
+
+            if (string.IsNullOrWhiteSpace(splits[1])) {
+                if (lines[i + 1].data.StartsWith('-')) {
+                    start.Add(splits[0].Replace('-', ' ').Trim(), ReadLinesAsList(NextLineLessOrEqual(lines, i + 1)));
+                    i++;
+                    while (i < lines.Count && lines[i].data.StartsWith('-')) { i++; }
+                    i--; // Don't really know what's up with this stuff.
+                } else {
+                    // Is dictionary
+                    start.Add(splits[0].Replace('-', ' ').Trim(), ReadLinesAsDictionary(NextLineLessOrEqual(lines, i + 1)));
+                    i++;
+                    while (i < lines.Count && lines[i].indentCount != currentIndent) { i++; }
+                    i--;
+                }
+            } else {
+                start.Add(splits[0].Replace('-', ' ').Trim(), ReadLineAsPrimitive(splits[1].Trim()));
+            }
+        }
+        return new ObjectConfigOption(start);
+    }
+
+    private IBaseConfigOption ReadLinesAsList(List<(int indentCount, string data)> lines) {
+        List<IBaseConfigOption> start = new List<IBaseConfigOption>();
+        int currentIndent = lines[0].indentCount;
+        for (int i = 0; i < lines.Count; i++) {
+            (int indentCount, string data) line = lines[i];
+            if (currentIndent < line.indentCount) {
+                start.Add(ReadLinesAsList(NextLineLessOrEqual(lines, i)));
+                while (i < lines.Count && lines[i].indentCount != currentIndent) { i++; }
+            } else {
+                string lineData = line.data.Replace('-', ' ');
+                if (string.IsNullOrWhiteSpace(lineData)) {
+                    continue;
+                }
+                if (lineData.Contains(':')) {
+                    start.Add(ReadLinesAsDictionary(NextLineLessOrEqual(lines, i, '-')));
+                    i++;
+                    while (i < lines.Count && !lines[i].data.StartsWith('-')) { i++; }
+                    i--; // Don't really know what's up with this stuff.
+                } else {
+                    start.Add(ReadLineAsPrimitive(lineData));
+                }
+            }
+        }
+        return new ArrayConfigOption(start);
+    }
+
+    private List<(int indentCount, string data)> NextLineLessOrEqual(List<(int indentCount, string data)> lines, int index, char startCharacter = '!') {
+        List<(int, string)> outList = new List<(int, string)>();
+        int indentCount = lines[index].indentCount;
+        for (int i = index; i < lines.Count; i++) {
+            if (lines[i].indentCount < indentCount || (i != index && lines[i].data.StartsWith(startCharacter))) {
+                break;
+            }
+            outList.Add(lines[i]);
+        }
+        return outList;
+    }
+
+    private IBaseConfigOption ReadLineAsPrimitive(string line) {
+        return new PrimitiveConfigOption(line);
     }
     #endregion
 }
