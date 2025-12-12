b1c85fb13e73e7f7e065ca703fe3fa47a3e400be
Cody <codyhowell44@gmail.com>
2025-12-11 20:36:00 -0700

    All TXT parser tests

--- HowlDev.IO.Text/Tests/HowlDev.IO.Text.Parsers.Tests/TXTParserTests.cs
+++ HowlDev.IO.Text/Tests/HowlDev.IO.Text.Parsers.Tests/TXTParserTests.cs
@@ -5,12 +5,6 @@ namespace HowlDev.IO.Text.Parsers.Tests;
 
 internal class TXTParserTests {
     [Test]
-    public async Task File1() {
-        List<(TextToken token, string value)> parsed = new(new TXTParser(File.ReadAllText("../../../data/TXT/File1.txt")));
-
-    }
-
-    [Test]
     public async Task String() {
         List<(TextToken token, string value)> parsed = new(new TXTParser(File.ReadAllText("../../../data/TXT/String.txt")));
         await Assert.That(parsed[0].token).IsEqualTo(TextToken.StartObject);
@@ -24,15 +18,49 @@ internal class TXTParserTests {
     [Test]
     public async Task MixedObject() {
         List<(TextToken token, string value)> parsed = new(new TXTParser(File.ReadAllText("../../../data/TXT/MixedObject.txt")));
+        await Assert.That(parsed[0].token).IsEqualTo(TextToken.StartObject);
+        await Assert.That(parsed[1].token).IsEqualTo(TextToken.KeyValue);
+        await Assert.That(parsed[2].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[3].token).IsEqualTo(TextToken.KeyValue);
+        await Assert.That(parsed[4].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[5].token).IsEqualTo(TextToken.KeyValue);
+        await Assert.That(parsed[6].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[7].token).IsEqualTo(TextToken.KeyValue);
+        await Assert.That(parsed[8].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[9].token).IsEqualTo(TextToken.KeyValue);
+        await Assert.That(parsed[10].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[11].token).IsEqualTo(TextToken.EndObject);
     }
 
     [Test]
     public async Task MixedArray() {
         List<(TextToken token, string value)> parsed = new(new TXTParser(File.ReadAllText("../../../data/TXT/MixedArray.txt")));
+        await Assert.That(parsed[0].token).IsEqualTo(TextToken.StartObject);
+        await Assert.That(parsed[1].token).IsEqualTo(TextToken.KeyValue);
+        await Assert.That(parsed[1].value).IsEqualTo("Mixed Array");
+        await Assert.That(parsed[2].token).IsEqualTo(TextToken.StartArray);
+        await Assert.That(parsed[3].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[4].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[5].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[6].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[7].token).IsEqualTo(TextToken.EndArray);
+        await Assert.That(parsed[8].token).IsEqualTo(TextToken.EndObject);
     }
 
     [Test]
     public async Task FourLineArray() {
         List<(TextToken token, string value)> parsed = new(new TXTParser(File.ReadAllText("../../../data/TXT/FourLineArray.txt")));
+        await Assert.That(parsed[0].token).IsEqualTo(TextToken.StartObject);
+        await Assert.That(parsed[1].token).IsEqualTo(TextToken.KeyValue);
+        await Assert.That(parsed[1].value).IsEqualTo("Four Line Array");
+        await Assert.That(parsed[2].token).IsEqualTo(TextToken.StartArray);
+        await Assert.That(parsed[3].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[4].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[5].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[6].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[7].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[8].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[9].token).IsEqualTo(TextToken.EndArray);
+        await Assert.That(parsed[10].token).IsEqualTo(TextToken.EndObject);
     }
 }
