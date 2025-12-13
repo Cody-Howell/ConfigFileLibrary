1fe15ead7485599a5a3cd53a0f599f6f132748a0
Cody-Howell <codyhowell44@gmail.com>
2025-12-12 18:31:59 -0700

    Updated config file

--- HowlDev.IO.Text/HowlDev.IO.Text.ConfigFile/TextConfigFile.cs
+++ HowlDev.IO.Text/HowlDev.IO.Text.ConfigFile/TextConfigFile.cs
@@ -13,6 +13,7 @@ namespace HowlDev.IO.Text.ConfigFile;
 public class TextConfigFile : IBaseConfigOption {
     private IBaseConfigOption option;
 
+    #region Option Exports
     /// <summary/>
     public BaseType type => option.type;
     /// <summary/>
@@ -47,6 +48,7 @@ public class TextConfigFile : IBaseConfigOption {
     public bool TryGet(string key, out IBaseConfigOption value) => option.TryGet(key, out value);
     /// <summary/>
     public bool Contains(string key) => option.Contains(key);
+    #endregion
 
     private List<string> acceptedExtensions = [".txt", ".yml", ".yaml", ".json"];
 
@@ -71,8 +73,7 @@ public class TextConfigFile : IBaseConfigOption {
                 option = ParseFileAsOption(new YAMLParser(file));
                 break;
             case ".json":
-                string fileValue = file.Replace('\r', ' ').Replace('\n', ' ');
-                option = ParseFileContents(fileValue);
+                option = ParseFileAsOption(new JSONParser(file));
                 break;
             default: throw new Exception("Extension error should be handled above. Extension was not recognized.");
         }
@@ -82,7 +83,7 @@ public class TextConfigFile : IBaseConfigOption {
     /// <param name="fileValue">JSON string</param>
     public static TextConfigFile ReadTextAsJSON(string fileValue) {
         TextConfigFile file = new TextConfigFile();
-        file.option = file.ParseFileContents(fileValue);
+        file.option = ParseFileAsOption(new JSONParser(fileValue));
         return file;
     }
 
@@ -123,122 +124,4 @@ public class TextConfigFile : IBaseConfigOption {
         Frame root = stack.Pop();
         return root.AsOption();
     }
-
-    #region JSON
-    private IBaseConfigOption ReadAsList(string file, ref int readingIndex) {
-        List<IBaseConfigOption> list = new List<IBaseConfigOption>();
-        readingIndex++;
-        int nextComma = 0;
-        int nextBracket = 1;
-
-        string subString = "";
-        while (readingIndex != 0) {
-            nextComma = file.IndexOf(',', readingIndex);
-            nextBracket = file.IndexOf(']', readingIndex);
-
-            if (readingIndex >= nextBracket) {
-                readingIndex += 2;
-                break;
-            }
-
-            if (nextComma < 0) {
-                subString = file[readingIndex..nextBracket].Replace('"', ' '); // Uses a "range" operator for the substring
-            } else {
-                subString = file[readingIndex..nextComma].Replace('"', ' '); // Uses a "range" operator for the substring
-            }
-
-            if (subString.TrimStart().StartsWith('[')) {
-                readingIndex = file.IndexOf('[', readingIndex);
-                list.Add(ReadAsList(file, ref readingIndex));
-            } else if (subString.TrimStart().StartsWith('{')) {
-                readingIndex = file.IndexOf('{', readingIndex);
-                list.Add(ReadAsDictionary(file, ref readingIndex));
-            } else {
-                list.Add(new PrimitiveConfigOption(subString.Replace(']', ' ')));
-                if (nextBracket > nextComma) {
-                    readingIndex = nextComma + 1;
-                } else {
-                    readingIndex = file.IndexOf(']', readingIndex);
-                }
-            }
-        }
-
-        return new ArrayConfigOption(list);
-    }
-
-    private IBaseConfigOption ReadAsDictionary(string file, ref int readingIndex) {
-        Dictionary<string, IBaseConfigOption> dict = new Dictionary<string, IBaseConfigOption>();
-        readingIndex++;
-        int nextComma = 0;
-        int nextBracket = 0;
-
-        string subString = "";
-        string key = "";
-        string value = "";
-        bool breakFlag = false;
-        while (readingIndex != 0) {
-            nextComma = file.IndexOf(',', readingIndex);
-            nextBracket = file.IndexOf('}', readingIndex);
-
-            if (nextComma < 0) {
-                subString = file[readingIndex..nextBracket].Replace('"', ' '); // Uses a "range" operator for the substring
-                int colonIndex = subString.IndexOf(':');
-                key = subString[0..colonIndex];
-                colonIndex++;
-                value = subString[colonIndex..];
-            } else {
-                subString = file[readingIndex..nextComma].Replace('"', ' ');
-                int colonIndex = subString.IndexOf(':');
-                if (colonIndex < 0) {
-                    readingIndex = nextComma + 1;
-                    break;
-                }
-                key = subString[0..colonIndex];
-                colonIndex++;
-                if (nextComma > nextBracket) {
-                    breakFlag = true;
-                    value = subString[colonIndex..(subString.Length - 1)];
-                } else {
-                    value = subString[colonIndex..];
-                }
-            }
-
-            if (readingIndex >= nextBracket) {
-                readingIndex += 2;
-                break;
-            }
-
-            if (value.TrimStart().StartsWith('[')) {
-                readingIndex = file.IndexOf('[', readingIndex);
-                dict.Add(key.Trim(), ReadAsList(file, ref readingIndex));
-                continue;
-            } else if (value.TrimStart().StartsWith('{')) {
-                readingIndex = file.IndexOf('{', readingIndex);
-                dict.Add(key.Trim(), ReadAsDictionary(file, ref readingIndex));
-                continue;
-            } else {
-                dict.Add(key.Trim(), new PrimitiveConfigOption(value.Replace('}', ' ')));
-            }
-
-            if (breakFlag) {
-                readingIndex = nextBracket + 2;
-                break;
-            }
-
-            readingIndex = nextComma + 1;
-        }
-        return new ObjectConfigOption(dict);
-    }
-
-    private IBaseConfigOption ParseFileContents(string file) {
-        int _ = 0; // Just to satisfy the compiler
-        if (file.StartsWith("[")) {
-            return ReadAsList(file, ref _);
-        } else if (file.StartsWith("{")) {
-            return ReadAsDictionary(file, ref _);
-        } else {
-            throw new InvalidDataException("JSON file must start with either [ or {");
-        }
-    }
-    #endregion
 }
