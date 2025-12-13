6c75b82f5a1ee205cd435cd4c9432fe9271c4e02
Cody-Howell <codyhowell44@gmail.com>
2025-12-12 18:04:13 -0700

    Updated COnfigFile

--- HowlDev.IO.Text/HowlDev.IO.Text.ConfigFile/TextConfigFile.cs
+++ HowlDev.IO.Text/HowlDev.IO.Text.ConfigFile/TextConfigFile.cs
@@ -68,22 +68,7 @@ public class TextConfigFile : IBaseConfigOption {
                 return;
             case ".yaml":
             case ".yml":
-                List<(int indentCount, string data)> lines = YAMLHelper.ReturnOrderedLines(file);
-
-                if (lines[0].data.StartsWith('-')) {
-                    // Read lines as list
-                    option = ReadLinesAsList(lines);
-                } else if (lines[0].data.Contains(':')) {
-                    // Read lines as dictionary
-                    option = ReadLinesAsDictionary(lines);
-                } else {
-                    // Read lines as primitive
-                    string line = "";
-                    foreach ((int indentCount, string data) in lines) {
-                        line += data + " ";
-                    }
-                    option = ReadLineAsPrimitive(line);
-                }
+                option = ParseFileAsOption(new YAMLParser(file));
                 break;
             case ".json":
                 string fileValue = file.Replace('\r', ' ').Replace('\n', ' ');
@@ -139,81 +124,7 @@ public class TextConfigFile : IBaseConfigOption {
         return root.AsOption();
     }
 
-    #region YAML
-    private IBaseConfigOption ReadLinesAsDictionary(List<(int indentCount, string data)> lines) {
-        Dictionary<string, IBaseConfigOption> start = new Dictionary<string, IBaseConfigOption>();
-        int currentIndent = lines[0].indentCount;
-        for (int i = 0; i < lines.Count; i++) {
-            (int indentCount, string data) line = lines[i];
-            string[] splits = line.data.Split(':');
-            if (splits.Length > 2) {
-                throw new FormatException($"Don't include multiple (:) on the same line. I read key: \"{splits[0].Trim()}\"");
-            }
-
-            if (string.IsNullOrWhiteSpace(splits[1])) {
-                if (lines[i + 1].data.StartsWith('-')) {
-                    start.Add(splits[0].Replace('-', ' ').Trim(), ReadLinesAsList(NextLineLessOrEqual(lines, i + 1)));
-                    i++;
-                    while (i < lines.Count && lines[i].data.StartsWith('-')) { i++; }
-                    i--; // Don't really know what's up with this stuff.
-                } else {
-                    // Is dictionary
-                    start.Add(splits[0].Replace('-', ' ').Trim(), ReadLinesAsDictionary(NextLineLessOrEqual(lines, i + 1)));
-                    i++;
-                    while (i < lines.Count && lines[i].indentCount != currentIndent) { i++; }
-                    i--;
-                }
-            } else {
-                start.Add(splits[0].Replace('-', ' ').Trim(), ReadLineAsPrimitive(splits[1].Trim()));
-            }
-        }
-        return new ObjectConfigOption(start);
-    }
-
-    private IBaseConfigOption ReadLinesAsList(List<(int indentCount, string data)> lines) {
-        List<IBaseConfigOption> start = new List<IBaseConfigOption>();
-        int currentIndent = lines[0].indentCount;
-        for (int i = 0; i < lines.Count; i++) {
-            (int indentCount, string data) line = lines[i];
-            if (currentIndent < line.indentCount) {
-                start.Add(ReadLinesAsList(NextLineLessOrEqual(lines, i)));
-                while (i < lines.Count && lines[i].indentCount != currentIndent) { i++; }
-            } else {
-                string lineData = line.data.Replace('-', ' ');
-                if (string.IsNullOrWhiteSpace(lineData)) {
-                    continue;
-                }
-                if (lineData.Contains(':')) {
-                    start.Add(ReadLinesAsDictionary(NextLineLessOrEqual(lines, i, '-')));
-                    i++;
-                    while (i < lines.Count && !lines[i].data.StartsWith('-')) { i++; }
-                    i--; // Don't really know what's up with this stuff.
-                } else {
-                    start.Add(ReadLineAsPrimitive(lineData));
-                }
-            }
-        }
-        return new ArrayConfigOption(start);
-    }
-
-    private List<(int indentCount, string data)> NextLineLessOrEqual(List<(int indentCount, string data)> lines, int index, char startCharacter = '!') {
-        List<(int, string)> outList = new List<(int, string)>();
-        int indentCount = lines[index].indentCount;
-        for (int i = index; i < lines.Count; i++) {
-            if (lines[i].indentCount < indentCount || (i != index && lines[i].data.StartsWith(startCharacter))) {
-                break;
-            }
-            outList.Add(lines[i]);
-        }
-        return outList;
-    }
-
-    private IBaseConfigOption ReadLineAsPrimitive(string line) {
-        return new PrimitiveConfigOption(line);
-    }
-    #endregion
     #region JSON
-
     private IBaseConfigOption ReadAsList(string file, ref int readingIndex) {
         List<IBaseConfigOption> list = new List<IBaseConfigOption>();
         readingIndex++;
