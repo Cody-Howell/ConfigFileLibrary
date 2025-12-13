63ff7cf1aa9a4dc8126bec8a25917538954e9cf9
Cody <codyhowell44@gmail.com>
2025-12-12 23:53:08 -0700

    Uncommented last JSON test :P

--- HowlDev.IO.Text/Tests/HowlDev.IO.Text.Parsers.Tests/JSONParserTests.cs
+++ HowlDev.IO.Text/Tests/HowlDev.IO.Text.Parsers.Tests/JSONParserTests.cs
@@ -105,18 +105,18 @@ internal class JSONParserTests {
     [Test]
     public async Task ObjectWithArray() {
         List<(TextToken token, string value)> parsed = new(new JSONParser(File.ReadAllText("../../../data/JSON/ObjectWithArray.json")));
-        // await Assert.That(parsed[0].token).IsEqualTo(TextToken.StartObject);
-        // await Assert.That(parsed[1].token).IsEqualTo(TextToken.KeyValue);
-        // await Assert.That(parsed[2].token).IsEqualTo(TextToken.StartArray);
-        // await Assert.That(parsed[3].token).IsEqualTo(TextToken.Primitive);
-        // await Assert.That(parsed[4].token).IsEqualTo(TextToken.Primitive);
-        // await Assert.That(parsed[5].token).IsEqualTo(TextToken.Primitive);
-        // await Assert.That(parsed[6].token).IsEqualTo(TextToken.EndArray);
-        // await Assert.That(parsed[7].token).IsEqualTo(TextToken.KeyValue);
-        // await Assert.That(parsed[8].token).IsEqualTo(TextToken.StartArray);
-        // await Assert.That(parsed[9].token).IsEqualTo(TextToken.Primitive);
-        // await Assert.That(parsed[10].token).IsEqualTo(TextToken.Primitive);
-        // await Assert.That(parsed[11].token).IsEqualTo(TextToken.EndArray);
-        // await Assert.That(parsed[12].token).IsEqualTo(TextToken.EndObject);
+        await Assert.That(parsed[0].token).IsEqualTo(TextToken.StartObject);
+        await Assert.That(parsed[1].token).IsEqualTo(TextToken.KeyValue);
+        await Assert.That(parsed[2].token).IsEqualTo(TextToken.StartArray);
+        await Assert.That(parsed[3].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[4].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[5].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[6].token).IsEqualTo(TextToken.EndArray);
+        await Assert.That(parsed[7].token).IsEqualTo(TextToken.KeyValue);
+        await Assert.That(parsed[8].token).IsEqualTo(TextToken.StartArray);
+        await Assert.That(parsed[9].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[10].token).IsEqualTo(TextToken.Primitive);
+        await Assert.That(parsed[11].token).IsEqualTo(TextToken.EndArray);
+        await Assert.That(parsed[12].token).IsEqualTo(TextToken.EndObject);
     }
 }
