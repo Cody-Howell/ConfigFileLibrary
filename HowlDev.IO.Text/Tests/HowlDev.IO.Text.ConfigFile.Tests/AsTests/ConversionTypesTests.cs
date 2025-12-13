51e6a953f1c520b19ffec4c64349f7f321f1add8
Cody-Howell <codyhowell44@gmail.com>
2025-12-13 15:57:03 -0700

    Added new test file.

---
+++ HowlDev.IO.Text/Tests/HowlDev.IO.Text.ConfigFile.Tests/AsTests/ConversionTypesTests.cs
@@ -0,0 +1,20 @@
+﻿using HowlDev.IO.Text.ConfigFile.Enums;
+using HowlDev.IO.Text.ConfigFile.Tests.Classes;
+
+namespace HowlDev.IO.Text.ConfigFile.Tests.AsTests;
+
+internal class ConversionTypesTests {
+    [Test]
+    public async Task ItAllWorks() {
+        // Make a txt file that includes all the types a Primitive can parse to
+        string txt = """
+        name: Jane
+        id: 23
+        """;
+        TextConfigFile reader = TextConfigFile.ReadTextAs(FileTypes.TXT, txt);
+
+        PersonRecord p = reader.AsConstructed<PersonRecord>();
+        await Assert.That(p.name).IsEqualTo("Jane");
+        await Assert.That(p.id).IsEqualTo(23);
+    }
+}
